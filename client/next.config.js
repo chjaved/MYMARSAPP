861a0c39c318c6c65db36952d9b75679514480de
chjaved <99609175+chjaved@users.noreply.github.com>
2024-08-14 00:23:46 -0700

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_ZEGO_APP_ID: "2017108866", // Enclosed in quotes
+    NEXT_PUBLIC_ZEGO_APP_ID: "2017108866", // Convert to string by adding quotes
     NEXT_PUBLIC_ZEGO_SERVER_ID: "478dae90644e0d69b9838aef2662d202",
   },
   images: {
